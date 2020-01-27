4c08220007dfb8b6cce16047b84f1ae5948f0e3d
mangesh <mlatekar@gmail.com>
2020-01-27 18:48:14 +0530

    MODIFIED:Use function bet() and totalDay()

--- gambling_simulator.sh
+++ gambling_simulator.sh
@@ -2,19 +2,34 @@ echo "Welcome to Gambling Simulator "
 STAKE=100
 BET=1
 cash=$STAKE
-function bet(){
+DAY=20
+totalAmount=0
+declare -A playingDay
+function bet()
+{
 	if [[ $((RANDOM%2)) -eq 1 ]]
 	 then
-		echo "You win"
+#		echo "You win"
 		cash=$((cash+$BET))
 	else
-		echo "You losse"
+#		echo "You losse"
 		cash=$((cash-$BET))
 	fi
 }
 resign=$(($STAKE*50/100))
-while [[ $cash -gt $(($STAKE-$resign)) && $cash -lt $(($STAKE+$resign)) ]]
-do 
-   bet
-done
-echo "resign with $cash"
+function totalDay()
+{
+for ((i=1; i<=$DAY; i++))
+	do
+    cash=$STAKE
+	   while [[ $cash -gt $(($STAKE-$resign)) && $cash -lt $(($STAKE+$resign)) ]]
+	    do 
+ 		  bet
+		 done
+	     playingDay[playingDay$i]=$(($STAKE-$cash))
+   		totalAmount=$((totalAmount+${playingDay[playingDay$i]}))	
+#	    echo ${playingDay[playingDay$i]} ":" $totalAmount
+	done
+}
+totalDay
+echo "20 Day amount with win/loose : " $totalAmount
