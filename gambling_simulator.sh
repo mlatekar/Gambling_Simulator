0421feb084a8f8cb8eca7a536c9e9332cf493259
mangesh <mlatekar@gmail.com>
2020-01-27 15:00:29 +0530

    ADDED:making first bet either win or loose

--- gambling_simulator.sh
+++ gambling_simulator.sh
@@ -1,4 +1,13 @@
 echo "Welcome to Gambling Simulator "
 STAKE=100
 BET=1
-
+cash=$STAKE
+if [[ $((RANDOM%2)) -eq 1 ]]
+	then
+	echo "You win"
+	((cash++))
+else
+	echo "You losse"
+	((cash--))
+	fi
+echo $cash
